6e98c4fc952efd3f30ad0820c7bb17e43ed5fef1
iaia <iaia72160@gmail.com>
2020-03-15 18:27:51 +0900

    Try sensor

--- app/src/main/java/com/example/furufuru/MainActivity.kt
+++ app/src/main/java/com/example/furufuru/MainActivity.kt
@@ -1,15 +1,54 @@
 package com.example.furufuru
 
+import android.content.Context
+import android.hardware.Sensor
+import android.hardware.SensorEvent
+import android.hardware.SensorEventListener
+import android.hardware.SensorManager
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import com.example.feature.IssueActivity
+import kotlin.math.sqrt
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var sensorManager: SensorManager
+    private var mAccel = 0f
+    private var mAccelCurrent = 0f
+    private var mAccelLast = 0f
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
 
-        startActivity(IssueActivity.createIntent(this))
+    override fun onResume() {
+        super.onResume()
+        val sensorManager = getSystemService(Context.SENSOR_SERVICE) as SensorManager
+        val sensor: Sensor? = sensorManager.getDefaultSensor(Sensor.TYPE_LINEAR_ACCELERATION)
+        sensorManager.registerListener(sensorEventListener, sensor, Sensor.TYPE_ACCELEROMETER, SensorManager.SENSOR_DELAY_NORMAL)
+    }
+
+    override fun onPause() {
+        super.onPause()
+        sensorManager.unregisterListener(sensorEventListener)
+    }
+
+    private val sensorEventListener = object: SensorEventListener {
+        override fun onAccuracyChanged(sensor: Sensor?, accuracy: Int) {
+        }
+
+        override fun onSensorChanged(event: SensorEvent) {
+            val x: Float = event.values[0]
+            val y: Float = event.values[1]
+            val z: Float = event.values[2]
+
+            mAccelLast = mAccelCurrent
+            mAccelCurrent = sqrt((x * x + y * y + z * z).toDouble()).toFloat()
+            val delta: Float = mAccelCurrent - mAccelLast
+            mAccel = mAccel * 0.9f + delta
+            if (mAccel > 12) {
+                Toast.makeText(applicationContext, "Shake event detected", Toast.LENGTH_SHORT).show()
+            }
+        }
     }
 }
--- feature/src/main/java/com/example/feature/IssueActivity.kt
+++ feature/src/main/java/com/example/feature/IssueActivity.kt
@@ -35,7 +35,6 @@ class IssueActivity : AppCompatActivity() {
             )
         }
 
-
         binding.viewModel = model
     }
 }
