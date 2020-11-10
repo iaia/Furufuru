8c606c173bcd767468134abd9f1efe2a869a584c
iaia <iaia72160@gmail.com>
2020-11-10 21:42:31 +0900

    Add issue repository test

---
+++ furufuru/src/test/java/dev/iaiabot/furufuru/data/repository/IssueRepositoryImplTest.kt
@@ -0,0 +1,47 @@
+package dev.iaiabot.furufuru.data.repository
+
+import dev.iaiabot.furufuru.data.entity.Issue
+import dev.iaiabot.furufuru.data.github.GithubService
+import dev.iaiabot.furufuru.util.FurufuruSettings
+import io.mockk.coEvery
+import io.mockk.every
+import io.mockk.mockk
+import kotlinx.coroutines.runBlocking
+import org.junit.jupiter.api.Assertions.fail
+import org.spekframework.spek2.Spek
+import org.spekframework.spek2.style.specification.describe
+import retrofit2.Response
+
+internal object IssueRepositoryImplTest : Spek({
+    lateinit var repository: IssueRepository
+    lateinit var furufuruSettings: FurufuruSettings
+    lateinit var service: GithubService
+
+    beforeGroup {
+        furufuruSettings = mockk {
+            every { githubRepositoryOwner } returns "iaia"
+            every { githubRepository } returns "Furufuru"
+        }
+        service = mockk()
+        repository = IssueRepositoryImpl(furufuruSettings, service)
+    }
+
+    describe("#post") {
+        val issue = Issue("title", "body")
+        beforeGroup {
+            coEvery { service.postIssue(any(), any(), any()) } returns Response.success(200, "ok")
+        }
+
+        context("例外が発生しない場合") {
+            it("投稿できる") {
+                runBlocking {
+                    try {
+                        repository.post(issue)
+                    } catch (e: Exception) {
+                        fail()
+                    }
+                }
+            }
+        }
+    }
+})
