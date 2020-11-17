c10cfec1223552a49c0be547a7f97ff76a049984
iaia <iaia72160@gmail.com>
2020-11-17 23:48:42 +0900

    Add screen shot getter

---
+++ furufuru/src/main/java/dev/iaiabot/furufuru/usecase/GetScreenShotUseCaseImpl.kt
@@ -0,0 +1,22 @@
+package dev.iaiabot.furufuru.usecase
+
+import dev.iaiabot.furufuru.data.repository.ScreenshotRepository
+import kotlinx.coroutines.delay
+
+internal class GetScreenShotUseCaseImpl(
+    private val screenshotRepository: ScreenshotRepository
+) : ScreenShotUseCase {
+
+    override suspend fun getScreenShot(): String? {
+        repeat(3) { repeatNum ->
+            val screenshot = screenshotRepository.get()
+            if (screenshot == null) {
+                delay(1000L * repeatNum)
+                return@repeat
+            } else {
+                return screenshot
+            }
+        }
+        return null
+    }
+}
