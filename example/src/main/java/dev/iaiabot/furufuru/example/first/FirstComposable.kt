a829d41a97ca7f76a4fbf0a4711cefacac866525
iaia <iaia72160@gmail.com>
2021-10-04 01:00:54 +0900

    Fix

--- example/src/main/java/dev/iaiabot/furufuru/example/first/FirstComposable.kt
+++ example/src/main/java/dev/iaiabot/furufuru/example/first/FirstComposable.kt
@@ -1,19 +1,23 @@
 package dev.iaiabot.furufuru.example.first
 
+import android.net.Uri
+import androidx.browser.customtabs.CustomTabsIntent
 import androidx.compose.foundation.layout.Column
 import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.platform.LocalContext
 import androidx.navigation.NavController
 import dev.iaiabot.furufuru.feature.ui.issue.IssueBodyTemplate
 
 @Composable
 fun FirstContent(
     navController: NavController,
-    openCustomTabs: () -> Unit = {}
 ) {
+    val context = LocalContext.current
+
     MaterialTheme {
         Column(horizontalAlignment = Alignment.CenterHorizontally) {
             Text(text = "hello")
@@ -28,7 +32,11 @@ fun FirstContent(
                 )
             )
             Button(
-                onClick = openCustomTabs
+                onClick = {
+                    CustomTabsIntent.Builder()
+                        .build()
+                        .launchUrl(context, Uri.parse("https://example.com"))
+                }
             ) {
                 Text("oepn custom tabs")
             }
@@ -45,15 +53,3 @@ fun FirstContent(
         }
     }
 }
-
-/*
-private fun openCustomTabs() {
-    val url = "http://example.com"
-    startActivity(
-        Intent(Intent.ACTION_VIEW, Uri.parse(url))
-    )
-    val builder = CustomTabsIntent.Builder()
-    val customTabsIntent: CustomTabsIntent = builder.build()
-    customTabsIntent.launchUrl(requireContext(), Uri.parse(url))
-}
- */
