4e9cabbd36db329eedd9511d2ca9bc5656fb0bb4
iaia <iaia72160@gmail.com>
2021-10-03 20:16:20 +0900

    Use compose for example

---
+++ example/src/main/java/dev/iaiabot/furufuru/example/Composable.kt
@@ -0,0 +1,37 @@
+package dev.iaiabot.furufuru.example
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material.Button
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.tooling.preview.Preview
+import dev.iaiabot.furufuru.feature.ui.issue.IssueBodyTemplate
+
+@Composable
+@Preview
+fun ExampleContent(
+    openCustomTabs: () -> Unit = {}
+) {
+    MaterialTheme {
+        Column(horizontalAlignment = Alignment.CenterHorizontally) {
+            Text(text = "hello")
+            Text(text = "hello")
+            Text(text = "hello")
+            Text(
+                text = IssueBodyTemplate.createBody(
+                    "",
+                    "user body",
+                    "image url",
+                    "file url"
+                )
+            )
+            Button(
+                onClick = openCustomTabs
+            ) {
+                Text("oepn custom tabs")
+            }
+        }
+    }
+}
--- example/src/main/java/dev/iaiabot/furufuru/example/MainActivity.kt
+++ example/src/main/java/dev/iaiabot/furufuru/example/MainActivity.kt
@@ -3,33 +3,25 @@ package dev.iaiabot.furufuru.example
 import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
+import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
 import androidx.browser.customtabs.CustomTabsIntent
-import dev.iaiabot.furufuru.example.databinding.ActivityMainBinding
-import dev.iaiabot.furufuru.feature.ui.issue.IssueBodyTemplate
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var binding: ActivityMainBinding
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        binding.tvHello.text = "hello"
-        binding.tvIssueTemplate.text = IssueBodyTemplate.createBody(
-            "",
-            "user body",
-            "image url",
-            "file url"
-        )
-        binding.btOpenWebview.setOnClickListener {
-            val url = "http://example.com"
-            startActivity(Intent(Intent.ACTION_VIEW, Uri.parse(url)))
-            val builder = CustomTabsIntent.Builder()
-            val customTabsIntent: CustomTabsIntent = builder.build()
-            customTabsIntent.launchUrl(this, Uri.parse(url))
+        setContent {
+            ExampleContent(::openCustomTabs)
         }
+    }
 
-        setContentView(binding.root)
+    private fun openCustomTabs() {
+        val url = "http://example.com"
+        startActivity(
+            Intent(Intent.ACTION_VIEW, Uri.parse(url))
+        )
+        val builder = CustomTabsIntent.Builder()
+        val customTabsIntent: CustomTabsIntent = builder.build()
+        customTabsIntent.launchUrl(this, Uri.parse(url))
     }
 }
