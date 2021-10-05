386f92b728e11555138acee4ccf2ef2d4e4467ae
iaia <iaia72160@gmail.com>
2021-10-05 21:21:58 +0900

    Add

--- example/src/main/java/dev/iaiabot/furufuru/example/first/FirstComposable.kt
+++ example/src/main/java/dev/iaiabot/furufuru/example/first/FirstComposable.kt
@@ -1,14 +1,20 @@
 package dev.iaiabot.furufuru.example.first
 
 import android.net.Uri
+import android.util.Log
 import androidx.browser.customtabs.CustomTabsIntent
 import androidx.compose.foundation.layout.Column
 import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.DisposableEffect
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalLifecycleOwner
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.LifecycleEventObserver
 import androidx.navigation.NavController
 import dev.iaiabot.furufuru.feature.ui.issue.IssueBodyTemplate
 
@@ -16,6 +22,41 @@ import dev.iaiabot.furufuru.feature.ui.issue.IssueBodyTemplate
 fun FirstContent(
     navController: NavController,
 ) {
+    val observer = remember {
+        LifecycleEventObserver { _, event ->
+            when (event) {
+                Lifecycle.Event.ON_CREATE -> {
+                    Log.d("koko", "first on_create")
+                }
+                Lifecycle.Event.ON_START -> {
+                    Log.d("koko", "first on_start")
+                }
+                Lifecycle.Event.ON_RESUME -> {
+                    Log.d("koko", "first on_resume")
+                }
+                Lifecycle.Event.ON_PAUSE -> {
+                    Log.d("koko", "first on_pause")
+                }
+                Lifecycle.Event.ON_STOP -> {
+                    Log.d("koko", "first on_stop")
+                }
+                Lifecycle.Event.ON_DESTROY -> {
+                    Log.d("koko", "first on_destroy")
+                }
+                Lifecycle.Event.ON_ANY -> {
+                }
+            }
+        }
+    }
+
+    val lifecycle = LocalLifecycleOwner.current.lifecycle
+    DisposableEffect(Unit) {
+        lifecycle.addObserver(observer)
+        onDispose {
+            lifecycle.removeObserver(observer)
+        }
+    }
+
     val context = LocalContext.current
 
     MaterialTheme {
--- example/src/main/java/dev/iaiabot/furufuru/example/second/SecondComposable.kt
+++ example/src/main/java/dev/iaiabot/furufuru/example/second/SecondComposable.kt
@@ -1,11 +1,17 @@
 package dev.iaiabot.furufuru.example.second
 
+import android.util.Log
 import androidx.compose.foundation.layout.Column
 import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.DisposableEffect
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.platform.LocalLifecycleOwner
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.LifecycleEventObserver
 import androidx.navigation.NavController
 import dev.iaiabot.furufuru.feature.ui.issue.IssueBodyTemplate
 
@@ -13,6 +19,40 @@ import dev.iaiabot.furufuru.feature.ui.issue.IssueBodyTemplate
 fun SecondContent(
     navController: NavController,
 ) {
+    val observer = remember {
+        LifecycleEventObserver { _, event ->
+            when (event) {
+                Lifecycle.Event.ON_CREATE -> {
+                    Log.d("koko", "second on_create")
+                }
+                Lifecycle.Event.ON_START -> {
+                    Log.d("koko", "second on_start")
+                }
+                Lifecycle.Event.ON_RESUME -> {
+                    Log.d("koko", "second on_resume")
+                }
+                Lifecycle.Event.ON_PAUSE -> {
+                    Log.d("koko", "second on_pause")
+                }
+                Lifecycle.Event.ON_STOP -> {
+                    Log.d("koko", "second on_stop")
+                }
+                Lifecycle.Event.ON_DESTROY -> {
+                    Log.d("koko", "second on_destroy")
+                }
+                Lifecycle.Event.ON_ANY -> {
+                }
+            }
+        }
+    }
+
+    val lifecycle = LocalLifecycleOwner.current.lifecycle
+    DisposableEffect(Unit) {
+        lifecycle.addObserver(observer)
+        onDispose {
+            lifecycle.removeObserver(observer)
+        }
+    }
     MaterialTheme {
         Column(horizontalAlignment = Alignment.CenterHorizontally) {
             Text(text = "second")
