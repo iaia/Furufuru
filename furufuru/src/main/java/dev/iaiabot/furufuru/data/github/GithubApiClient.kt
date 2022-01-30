09c686a5239d4d2700c4da73447f8294794a8e2b
iaia <iaia72160@gmail.com>
2022-01-30 18:55:44 +0900

    Set time out

--- furufuru/src/main/java/dev/iaiabot/furufuru/data/github/GithubApiClient.kt
+++ furufuru/src/main/java/dev/iaiabot/furufuru/data/github/GithubApiClient.kt
@@ -10,8 +10,11 @@ import okhttp3.OkHttpClient
 import okhttp3.Request
 import org.koin.java.KoinJavaComponent.inject
 import retrofit2.Retrofit
+import java.util.concurrent.TimeUnit
 
 internal object GithubApiClient {
+    private const val TIMEOUT_MILLI_SEC = 10_000L
+
     private val GITHUB_SETTINGS: GithubSettings by inject(GithubSettings::class.java)
 
     fun build(): GithubService {
@@ -21,17 +24,21 @@ internal object GithubApiClient {
 
     private fun buildRetrofit(): Retrofit {
         val contentType = MediaType.parse("application/json")!!
-        val client = OkHttpClient.Builder().addInterceptor { chain ->
-            val request = chain.request()
-            val buffer = okio.Buffer()
-            request.body()?.writeTo(buffer)
-            Log.d("Furufuru request body", buffer.readUtf8())
+        val client = OkHttpClient.Builder()
+            .connectTimeout(TIMEOUT_MILLI_SEC, TimeUnit.MILLISECONDS)
+            .writeTimeout(TIMEOUT_MILLI_SEC, TimeUnit.MILLISECONDS)
+            .readTimeout(TIMEOUT_MILLI_SEC, TimeUnit.MILLISECONDS)
+            .addInterceptor { chain ->
+                val request = chain.request()
+                val buffer = okio.Buffer()
+                request.body()?.writeTo(buffer)
+                Log.d("Furufuru request body", buffer.readUtf8())
 
-            val newRequest: Request = chain.request().newBuilder()
-                .addHeader("Authorization", "token ${GITHUB_SETTINGS.githubApiToken}")
-                .build()
-            chain.proceed(newRequest)
-        }.build()
+                val newRequest: Request = chain.request().newBuilder()
+                    .addHeader("Authorization", "token ${GITHUB_SETTINGS.githubApiToken}")
+                    .build()
+                chain.proceed(newRequest)
+            }.build()
         return Retrofit.Builder().run {
             client(client)
             addConverterFactory(
