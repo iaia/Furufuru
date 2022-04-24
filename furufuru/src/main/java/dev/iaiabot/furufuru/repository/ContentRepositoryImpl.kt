b08cfcde1a6b3408bbdbce4a2e3c66eeee088bfe
iaia <iaia72160@gmail.com>
2022-04-24 23:43:32 +0900

    Fix

--- furufuru/src/main/java/dev/iaiabot/furufuru/repository/ContentRepositoryImpl.kt
+++ furufuru/src/main/java/dev/iaiabot/furufuru/repository/ContentRepositoryImpl.kt
@@ -4,14 +4,12 @@ import dev.iaiabot.furufuru.data.entity.ContentImageUrls
 import dev.iaiabot.furufuru.data.github.GithubService
 import dev.iaiabot.furufuru.data.github.request.Content
 import dev.iaiabot.furufuru.util.GithubSettings
-import kotlinx.serialization.ExperimentalSerializationApi
 
 internal class ContentRepositoryImpl(
     private val settings: GithubSettings,
     private val service: GithubService,
 ) : ContentRepository {
 
-    @ExperimentalSerializationApi
     override suspend fun post(content: Content, path: String): ContentImageUrls? {
         service.postContent(
             settings.githubRepositoryOwner,
--- furufuru/src/main/java/dev/iaiabot/furufuru/repository/IssueRepositoryImpl.kt
+++ furufuru/src/main/java/dev/iaiabot/furufuru/repository/IssueRepositoryImpl.kt
@@ -3,14 +3,12 @@ package dev.iaiabot.furufuru.repository
 import dev.iaiabot.furufuru.data.github.GithubService
 import dev.iaiabot.furufuru.data.github.request.Issue
 import dev.iaiabot.furufuru.util.GithubSettings
-import kotlinx.serialization.ExperimentalSerializationApi
 
 internal class IssueRepositoryImpl(
     private val settings: GithubSettings,
     private val service: GithubService,
 ) : IssueRepository {
 
-    @ExperimentalSerializationApi
     override suspend fun post(issue: Issue) {
         service.postIssue(
             settings.githubRepositoryOwner,
