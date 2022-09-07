c4597b7f2b43da54a75ab8af7e6d29b024ad1e89
iaia <iaia72160@gmail.com>
2022-09-07 13:23:17 +0900

    Add

--- furufuru/src/test/java/dev/iaiabot/furufuru/feature/ExampleUnitTest.kt
+++ furufuru/src/test/java/dev/iaiabot/furufuru/feature/ExampleUnitTest.kt
@@ -1,16 +1,33 @@
 package dev.iaiabot.furufuru.feature
 
+import dev.iaiabot.furufuru.usecase.MyExtension
 import org.junit.Assert.assertEquals
-import org.junit.Test
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.extension.ExtendWith
 
 /**
  * Example local unit test, which will execute on the development machine (host).
  *
  * See [testing documentation](http://d.android.com/tools/testing).
  */
+
+@ExtendWith(MyExtension::class)
 class ExampleUnitTest {
     @Test
     fun addition_isCorrect() {
+        println("test1")
+        assertEquals(4, 2 + 2)
+    }
+
+    @Test
+    fun hoge() {
+        println("test2")
+        assertEquals(4, 2 + 2)
+    }
+
+    @Test
+    fun fuga() {
+        println("test3")
         assertEquals(4, 2 + 2)
     }
 }
