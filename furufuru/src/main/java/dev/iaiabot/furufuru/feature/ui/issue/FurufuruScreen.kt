14b03b211dba53c069e66b205b4a9388022dc4ab
“masato1230” <0ht3851f11t426k@ezweb.ne.jp>
2024-02-16 18:20:08 +0900

    Delete unused imports

--- furufuru/src/main/java/dev/iaiabot/furufuru/feature/ui/issue/FurufuruScreen.kt
+++ furufuru/src/main/java/dev/iaiabot/furufuru/feature/ui/issue/FurufuruScreen.kt
@@ -2,17 +2,13 @@ package dev.iaiabot.furufuru.feature.ui.issue
 
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material.*
+import androidx.compose.material.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 
